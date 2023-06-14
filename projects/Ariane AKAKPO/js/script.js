8cc9911d0a5d2606043c8d9f284b73bcca8db4c2
graciari2000 <92252649+graciari2000@users.noreply.github.com>
2023-06-15 00:14:33 +0100

    navbar again

--- projects/Ariane AKAKPO/js/script.js
+++
@@ -1,8 +0,0 @@
-var colorBoxes = document.querySelectorAll('.color-box');
-var selectedColor = document.getElementById('selected-color');
-colorBoxes.forEach(function(colorBox) {
-colorBox.addEventListener('click', function() {
-    var color = window.getComputedStyle(colorBox).backgroundColor;
-    selectedColor.style.backgroundColor = color;
-});
-});
