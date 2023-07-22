82df3f1971484fc64aaca8944b6272b076d4235a
graciari2000 <92252649+graciari2000@users.noreply.github.com>
2023-07-22 20:25:38 +0100

    projects

---
+++ projects/The hangman/hangman.py
@@ -0,0 +1,53 @@
+import random
+
+def choose_word():
+    words = ["apple", "banana", "cherry", "date", "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon"]
+    return random.choice(words)
+
+def display_word(word, guessed_letters):
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter
+        else:
+            display += "_"
+    return display
+
+def hangman():
+    word_to_guess = choose_word()
+    guessed_letters = []
+    attempts = 6
+
+    print("Welcome to Hangman!")
+    print("You have 6 attempts to guess the word.")
+    
+    while attempts > 0:
+        print("\n" + display_word(word_to_guess, guessed_letters))
+        guess = input("Enter a letter: ").lower()
+
+        if len(guess) != 1 or not guess.isalpha():
+            print("Invalid input. Please enter a single letter.")
+            continue
+
+        if guess in guessed_letters:
+            print("You already guessed that letter.")
+            continue
+
+        guessed_letters.append(guess)
+
+        if guess not in word_to_guess:
+            attempts -= 1
+            print("Incorrect guess. Attempts left:", attempts)
+
+        if set(word_to_guess) == set(guessed_letters):
+            print("\nCongratulations! You guessed the word:", word_to_guess)
+            break
+
+    else:
+        print("\nGame over! The word was:", word_to_guess)
+
+if __name__ == "__main__":
+    hangman()
+
+
+    
